51ebb479f5463d552889ed7bceb19b3a6f3c76bf
Julien Mars <julien.mars@epitech.eu>
2024-05-16 00:34:47 +0200

    [~] Edited epytodo.sql fix something in my user table

--- epytodo.sql
+++ epytodo.sql
@@ -5,10 +5,11 @@ CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    name VARCHAR(100) NOT NULL,
     firstname VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 
 CREATE TABLE todo (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -16,7 +17,7 @@ CREATE TABLE todo (
     description TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     due_time DATETIME NOT NULL,
-    status ENUM('not started', 'todo', 'in progress', 'done') DEFAULT 'not started',
     user_id INT,
+    status ENUM('not started', 'todo', 'in progress', 'done') DEFAULT 'not started',
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
