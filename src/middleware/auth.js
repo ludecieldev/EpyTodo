a2f16eacfd88f28aa351a64d62c1fe1ed2e8d198
Julien Mars <julien.mars@epitech.eu>
2024-05-16 00:30:43 +0200

    [~] Edited middleware/auth.js added error handling for the token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
-    const token = req.headers['authorization']?.split(' ')[1]; // Bearer Token
-    if (token == null) return res.sendStatus(401);
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
+    if (!token) {
+        return res.status(401).json({ msg: 'No token provided' });
+    }
     jwt.verify(token, process.env.SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
+        if (err) {
+            console.error("JWT verification error:", err);
+            return res.status(403).json({ msg: 'Token is invalid' });
+        }
+        req.user = { user_id: user.user_id, email: user.email };
         next();
     });
 }
