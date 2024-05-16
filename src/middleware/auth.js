cef40eb47cbe5d0ebbb6156089d522f5d070412f
Julien Mars <julien.mars@epitech.eu>
2024-05-16 02:34:18 +0200

    [~] Fix src/middleware/auth.js wrong error message

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,13 +5,14 @@ function authenticateToken(req, res, next) {
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
-        return res.status(401).json({ msg: 'No token provided' });
+        return res.status(401).json({ msg: 'No token, authorization denied' });
     }
     jwt.verify(token, process.env.SECRET, (err, user) => {
         if (err) {
             console.error("JWT verification error:", err);
-            return res.status(403).json({ msg: 'Token is invalid' });
+            return res.status(403).json({ msg: 'Token is not valid' });
         }
+        console.log('Authenticated user:', user);
         req.user = { user_id: user.user_id, email: user.email };
         next();
     });
