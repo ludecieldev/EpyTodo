13cba65dd872dd04cd64e762806b3cb1736114f6
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-05-17 13:24:27 +0200

    fix: updated auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,17 +3,15 @@ const jwt = require('jsonwebtoken');
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-
     if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
+        return res.status(401).json({ msg: "Unauthorized" });
     }
+
     jwt.verify(token, process.env.SECRET, (err, user) => {
         if (err) {
-            console.error("JWT verification error:", err);
-            return res.status(403).json({ msg: 'Token is not valid' });
+            return res.status(403).json({ msg: "Forbidden" });
         }
-        console.log('Authenticated user:', user);
-        req.user = { user_id: user.user_id, email: user.email };
+        req.user = user;
         next();
     });
 }
