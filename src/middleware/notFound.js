b0458db9cf0e087a34c13221e3f38dbb3d980768
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-05-17 13:24:41 +0200

    fix updated notFound.js

--- src/middleware/notFound.js
+++ src/middleware/notFound.js
@@ -1,9 +1,7 @@
 function notFound(req, res, next) {
-    res.status(404).json({
-        status: 404,
-        error: 'Not Found',
-        message: 'The requested resource was not found on this server.'
-    });
+    res.status(404);
+    const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+    next(error);
 }
 
 module.exports = notFound;
